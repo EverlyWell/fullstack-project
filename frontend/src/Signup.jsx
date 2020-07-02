ae7d5a7eb67211abe0c78497b635dea0d8089dfb
Jordan Tran <jordantran@scalefactor.com>
2020-07-01 22:01:56 -0500

    Add registration action to Signup component

--- frontend/src/Signup.jsx
+++ frontend/src/Signup.jsx
@@ -1,24 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Container from '@material-ui/core/Container'
 import Typography from '@material-ui/core/Typography'
 import TextField from '@material-ui/core/TextField'
 import Link from '@material-ui/core/Link'
 import Button from '@material-ui/core/Button'
+import { connect } from 'react-redux'
+import { setAuthToken } from './actions/index'
+import { registerUser } from './services/user'
+import Cookies from 'js-cookie'
+import { useHistory } from 'react-router-dom'
 
-function Signup() {
+function Signup({ setAuthToken }) {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [submitting, setSubmitting] = useState(false)
+
+  let history = useHistory()
+
+  function updateEmail(event) {
+    setEmail(event.target.value)
+  }
+
+  function updatePassword(event) {
+    setPassword(event.target.value)
+  }
+
+  function register(event) {
+    event.preventDefault()
+    setSubmitting(true)
+    registerUser(email, password)
+    .then(({ data }) => {
+      let authToken = data.auth
+      Cookies.set('userToken', authToken)
+      setAuthToken(authToken)
+      history.push('/')
+    })
+    .catch((e) => {
+      console.error(e)
+    })
+    .finally(() => {
+      setSubmitting(false)
+    })
+  }
+
   return (
     <Container maxWidth="xs">
       <Typography component="h1" variant="h5">
         Sign Up
       </Typography>
 
-      <form className="login-form" noValidate>
+      <form className="login-form" noValidate onSubmit={register}>
         <TextField
           required
           fullWidth
           autoFocus
           label="Email Address"
           name="email"
+          value={email}
+          onChange={updateEmail}
         />
 
         <TextField
@@ -27,6 +66,8 @@ function Signup() {
           name="password"
           label="Password"
           type="password"
+          value={password}
+          onChange={updatePassword}
         />
 
         <Button
@@ -35,6 +76,7 @@ function Signup() {
           variant="contained"
           color="primary"
           className="submit-button"
+          disabled={submitting}
         >
           Sign Up
         </Button>
@@ -47,4 +89,7 @@ function Signup() {
   )
 }
 
-export default Signup
+export default connect(
+  null,
+  { setAuthToken }
+)(Signup)
