f06c5335391882f2099cc9050758f6b0228770bb
Todd Vrba <todd.vrba@gmail.com>
2020-08-02 15:03:32 -0500

    Add logout functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,16 +1,34 @@
 import React from 'react';
-import { withRouter } from 'react-router';
+import { withRouter, useHistory } from 'react-router';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import { useSelector, useDispatch } from 'react-redux';
+import * as Cookies from 'js-cookie';
+import { RootState } from '../store/rootReducer';
+import { updateToken } from '../store/auth/actions';
 
 const Header = (props: any) => {
+  const { token } = useSelector(
+    (state: RootState) => state.auth
+  );
   const { location } = props;
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const logout = () => {
+    Cookies.remove('userToken')
+    dispatch(updateToken(''));
+    history.push('/login')
+  };
+
   return (
     <Navbar bg="light" variant="light">
-      <Nav activeKey={location.pathname}>
+      <Navbar.Brand href="#home">Giphy Search</Navbar.Brand>
+      <Nav activeKey={location.pathname} className="mr-auto">
         <Nav.Link href="/home">Home</Nav.Link>
         <Nav.Link href="/favorites">Favorites</Nav.Link>
       </Nav>
+      { token && <Nav.Link onClick={logout}>Log Out</Nav.Link> }
     </Navbar>
   );
 };
