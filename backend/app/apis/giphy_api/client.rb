994de17476e1c4c83cb4f84717d8178108ff8757
Todd Vrba <todd.vrba@gmail.com>
2020-08-01 21:53:46 -0500

    Initial setup of API endpoint
    - Create client to abstract the third-party API calls.
    - Use client within our own GET endpoint to retrieve images from GIPHY.
    - Setup dot-env to use environment files for the GIPHY API key.

---
+++ backend/app/apis/giphy_api/client.rb
@@ -0,0 +1,57 @@
+module GiphyApi
+  class Client
+    include HttpStatusCodes
+    include ApiExceptions
+
+    BASE_URL = 'https://api.giphy.com/v1/gifs'.freeze
+
+    def image_search(params = {})
+      request(
+        http_method: :get,
+        endpoint: 'search',
+        params: params
+      )
+    end
+
+    private
+
+    def client
+      @_client ||= Faraday.new(BASE_URL) do |faraday|
+        faraday.request :url_encoded
+        faraday.adapter Faraday.default_adapter
+        faraday.params['api_key'] = ENV['GIPHY_API_KEY']
+      end
+    end
+
+    def request(http_method:, endpoint:, params: {})
+      response = client.public_send(http_method, endpoint, params)
+      parsed_response = Oj.load(response.body)
+
+      return parsed_response if request_successful?(response)
+
+      raise error_class(response), "HTTP Code: #{response.status}, response: #{response.body}"
+    end
+
+    def error_class(response)
+      case response.status
+      when HTTP_BAD_REQUEST_CODE
+        BadRequestError
+      when HTTP_UNAUTHORIZED_CODE
+        UnauthorizedError
+      when HTTP_FORBIDDEN_CODE
+        ForbiddenError
+      when HTTP_NOT_FOUND_CODE
+        NotFoundError
+      when HTTP_UNPROCESSABLE_ENTITY_CODE
+        UnprocessableEntityError
+      else
+        ApiError
+      end
+    end
+
+    def request_successful?(response)
+      response.status == HTTP_OK_CODE
+    end
+
+  end
+end
---
+++ backend/app/controllers/api/v1/images_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::ImagesController < ApplicationController
+
+  # GET /api/v1/images
+  def index
+    giphy_api_client = GiphyApi::Client.new
+
+    begin
+      resp = giphy_api_client.image_search(search_params)
+
+      render json: resp
+    rescue => e
+      render json: e, status: 400
+    end
+
+  end
+
+  private
+
+  def search_params
+    params.permit(:q)
+  end
+end
