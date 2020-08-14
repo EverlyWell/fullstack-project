8e09baaa8479437308a27886480b438a7dd39276
caiyun liu <cliu.aurora@gmail.com>
2020-08-14 16:55:36 -0400

    change knock version as it got broken for rails 6

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
   include Knock::Authenticable
-  # before_action :authenticate_user
+  before_action :authenticate_user
   # undef_method :current_user
 end
