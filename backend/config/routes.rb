77d9f38bc1962e123941327d0c1d6cacd7a308be
kudarepos <workstuff2016@gmail.com>
2020-08-18 17:10:32 -0400

    Routes initial checkin

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
+    namespace :v1 do
+      resources :cats, format: 'json' do
+        collection do
+          get 'breeds'
+          get 'categories'
+          get 'search'
+          get 'favorites'
+          get 'search_by_breed'
+        end
+      end
+    end
   end
 end
