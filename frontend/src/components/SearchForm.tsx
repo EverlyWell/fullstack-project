aeedd9abc49952c963e64da581c124638f84a168
James Pierce <jamespierce@fastmail.com>
2020-08-21 06:25:54 -0500

    SearchForm submits query to api

--- frontend/src/components/SearchForm.tsx
+++ frontend/src/components/SearchForm.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const SearchForm = () => {
+  const [submitButtonText, setSubmitButtonText] = useState('Search');
+
+  function fetchGiphyData(q: string) {
+    return fetch(`http://localhost:3010/api/search?q=${q}`);
+  }
+
   function handleSubmit(e: React.SyntheticEvent) {
     e.preventDefault();
+    setSubmitButtonText('Searching...');
     const formElement = e.target as HTMLFormElement;
     const formData = new FormData(formElement);
-    console.log(formData.get('q'));
+    const q = formData.get('q') as string;
+    fetchGiphyData(q).then(data => {
+      console.log(data);
+    });
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <input type="text" name="q" placeholder="search" />
-      <input type="submit" value="Search" />
+      <input type="submit" value={submitButtonText} />
     </form>
-  )
-}
+  );
+};
 
 export default SearchForm;
