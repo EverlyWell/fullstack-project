669f86dd312363d4acf704b8b3633142d3beaf95
Adam Kahtava <adam.kahtava@varsitytutors.com>
2020-08-24 15:46:51 -0700

    Support for respond_with and default json responses using the responders gem

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
+require "application_responder"
+
 class ApplicationController < ActionController::Base
+  # Making use of responders, for more details: https://github.com/heartcombo/responders
+  self.responder = ApplicationResponder
+
+  # Allowing json responses for use with the 'respond_with' method
+  respond_to :json
+
+  protect_from_forgery with: :null_session # ToDo: implement alternate security / authentication
 end
