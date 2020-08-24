2095ad3e0dd56777b2ed003510d6f88db8067635
CJ Jessett <cjessett@gmail.com>
2020-08-24 10:37:32 -0700

    Implement search feature

---
+++ frontend/src/Search.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+ 
+import Search from './Search';
+ 
+describe('Search', () => {
+  test('renders Search component', () => {
+    render(<Search />);
+ 
+    expect(screen.getByText('Search for a Gif')).toBeInTheDocument();
+    expect(screen.getByRole('textbox')).toBeInTheDocument();
+  });
+});
---
+++ frontend/src/Search.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { Grid } from '@giphy/react-components';
+import { GiphyFetch } from '@giphy/js-fetch-api';
+
+const gf = new GiphyFetch(process.env.REACT_APP_GIPHY_API_KEY!)
+const LIMIT = 10;
+
+export default function Search() {
+  const [term, setTerm] = useState('animals');
+    
+  const handleChange = (e: any) => setTerm(e.target.value);
+  
+  const fetchGifs = (offset: number) => gf.search(term, { offset, limit: LIMIT });
+
+  return (
+    <div>
+      <h1 style={{ margin: '1em 0 0 1em' }}>Search for a Gif</h1>
+      <section className="header">
+        <input type="text" placeholder="Search" value={term} onChange={handleChange} />
+      </section>
+      <Grid
+        key={term}
+        columns={3}
+        fetchGifs={fetchGifs}
+        gutter={4}
+        noResultsMessage={<div>No results</div>}
+        width={1220}
+      />
+    </div>
+  )
+}
