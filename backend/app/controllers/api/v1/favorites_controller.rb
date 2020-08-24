6ac9250a8eedf8ef54c57382d5ca24eee2590d01
Adam Kahtava <adam.kahtava@varsitytutors.com>
2020-08-24 14:30:56 -0700

    Support for modifying favorites

--- backend/app/controllers/api/v1/favorites_controller.rb
+++ backend/app/controllers/api/v1/favorites_controller.rb
@@ -4,6 +4,20 @@ module Api
       def show
         render json: Favorite::Base.all
       end
+
+      def destroy
+        Favorite::Base.where(slug: favorite_params[:favorite][:slug], type: favorite_params[:favorite][:type]).delete_all
+      end
+
+      def create
+        Favorite::Base.create(favorite_params[:favorite])
+      end
+
+      private
+
+      def favorite_params
+        params.permit(:favorite => [:slug, :type])
+      end
     end
   end
 end
--- backend/test/controllers/api/v1/favorites_controller_test.rb
+++ backend/test/controllers/api/v1/favorites_controller_test.rb
@@ -1,16 +1,36 @@
 require 'test_helper'
 
 class FavoritesControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @favorite = Favorite::Giphy.create(slug: 'abc123')
-  end
+  test "show favorites" do
+    favorite = Favorite::Giphy.create(slug: 'abc123')
 
-  test "returns favorites" do
     get '/api/v1/favorites'
 
     response_body = JSON.parse(response.body).first
-    assert_equal @favorite.slug, response_body['slug']
+    assert_equal favorite.slug, response_body['slug']
 
-    assert_response 200
+    assert_response :success
+  end
+
+  test "create favorite" do
+    expected_attributes = { 'type' => 'Favorite::Giphy', 'slug' => 'zyx321' }
+
+    post '/api/v1/favorites', params: { favorite: expected_attributes }
+
+    last_favorite_with_expected_attributes = Favorite::Base.last.attributes.slice('type', 'slug')
+    assert_equal expected_attributes, last_favorite_with_expected_attributes
+
+    assert_response 204
+  end
+
+  test "delete favorite" do
+    expected_slug = 'lmn567'
+    Favorite::Giphy.create(slug: expected_slug)
+
+    delete '/api/v1/favorites', params: { favorite: { type: 'Favorite::Giphy', slug: expected_slug }}
+
+    refute Favorite::Base.where(slug: expected_slug).any?
+
+    assert_response 204
   end
 end
