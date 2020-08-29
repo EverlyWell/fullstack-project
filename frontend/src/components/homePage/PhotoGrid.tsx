5919d2e4376f9a67b72c1c180000fa3a38e07c43
Mitchel <mitchconstantin@gmail.com>
2020-08-29 13:31:50 -0500

    add naive frontend approach to favoriting

--- frontend/src/components/homePage/PhotoGrid.tsx
+++ frontend/src/components/homePage/PhotoGrid.tsx
@@ -1,21 +1,45 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState } from "react";
 import { Photo } from "./HomePage";
-import { CircularProgress } from "@material-ui/core";
+import { CircularProgress, Grid } from "@material-ui/core";
+
+const setIsFavorite = (id: string, status: boolean) => {
+  localStorage.setItem(id, String(status));
+};
+
+const toggleFavorite = (id: string) => {
+  const isFavorite = getIsFavorite(id);
+  setIsFavorite(id, !isFavorite);
+};
+
+const getIsFavorite = (id: string) => {
+  return !!localStorage.getItem(id);
+};
 
 interface Props {
   photos: Photo[];
   loading: boolean;
 }
+
 export const PhotoGrid = ({ photos, loading }: Props) => {
+  // const [favorites, setFavorites] = useState();
   if (loading) return <CircularProgress />;
   if (!photos.length)
     return <div> type in a search phrase to see cat photos </div>;
   return (
     <div>
       Here are your pictures:
-      {photos.map((photo) => (
-        <img key={photo.id} src={photo.url} />
-      ))}
+      <Grid spacing={3} container>
+        {photos.map((photo) => (
+          <Grid key={photo.id} item>
+            <img
+              style={getIsFavorite(photo.id) ? { border: "dashed red" } : {}}
+              onClick={() => toggleFavorite(photo.id)}
+              width="150px"
+              src={photo.url}
+            />
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 };
