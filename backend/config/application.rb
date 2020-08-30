f2b2cf8129d8f2cc7c9dde7ecc580b8f08377015
Mitchel <mitchconstantin@gmail.com>
2020-08-30 10:17:11 -0500

    allow all CORS requests on backend

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -11,6 +11,11 @@ module Backend
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.action_dispatch.default_headers = {
+    'Access-Control-Allow-Origin' => '*',
+    'Access-Control-Request-Method' => %w{GET POST OPTIONS DELETE}.join(",")
+  }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
