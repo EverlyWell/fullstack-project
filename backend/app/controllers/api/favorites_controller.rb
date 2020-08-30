7469dc816708dfc0c934e14855610812654c54b6
Mitchel <mitchconstantin@gmail.com>
2020-08-30 09:58:19 -0500

    add logic to favorites_controller for read, create, delete

--- backend/app/controllers/api/favorites_controller.rb
+++ backend/app/controllers/api/favorites_controller.rb
@@ -1,7 +1,27 @@
 class Api::FavoritesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
-    favorites = Favorite.all
+    favorites = Favorite.where(user_id: params[:user_id])
     render json: favorites
+  end
+
+  def create
+     @favorite = Favorite.create!(favorite_params)
+     render json: :created
+  end
+
+  def destroy
+    @favorite = Favorite.find_by(user_id: params[:user_id], photo_id: params[:photo_id])
+    @favorite.destroy
+    head :no_content
+  end
+
+    private
+
+  def favorite_params
+    # whitelist params
+    params.permit(:user_id, :photo_id, :photo_url)
   end
 
 end
