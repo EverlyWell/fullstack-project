98c0cae791a7e487d70de4ebdc92c3c0c2e00fc2
lilirdz <lilimrod@gmail.com>
2021-01-16 21:16:10 -0600

    cleaning up on the backend

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :favorites
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
+    get 'gifs/search', to: 'gif#search'
+    resources :favorites
+
   end
 end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -15,13 +15,6 @@ ActiveRecord::Schema.define(version: 2021_01_16_185621) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "favorites", force: :cascade do |t|
     t.string "giphy_id"
     t.string "title"
@@ -29,76 +22,6 @@ ActiveRecord::Schema.define(version: 2021_01_16_185621) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["giphy_id"], name: "index_favorites_on_giphy_id", unique: true
-  end
-
-  create_table "prospective_schools", force: :cascade do |t|
-    t.integer "school_id"
-    t.integer "user_id"
-    t.integer "category_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "resource_categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "resources", force: :cascade do |t|
-    t.string "name"
-    t.string "organization_url"
-    t.string "about_snippet"
-    t.integer "resource_category_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "school_ambassadors", force: :cascade do |t|
-    t.string "name"
-    t.string "major"
-    t.integer "school_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "school_years", force: :cascade do |t|
-    t.integer "school_id"
-    t.integer "year"
-    t.float "admissions_rate"
-    t.integer "enrollment_size"
-    t.float "demographics_black"
-    t.float "demographics_hispanic"
-    t.float "demographics_asian"
-    t.float "demographics_nhpi"
-    t.float "demographics_white"
-    t.integer "tuition_in_state"
-    t.integer "tuition_out_of_state"
-    t.float "retention_rate"
-    t.float "first_gen_total_percentage"
-    t.float "first_gen_median_debt"
-    t.integer "first_gen_4_year_completion"
-    t.integer "first_gen_6_year_completion"
-    t.integer "alumni_earnings_10_years"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "schools", force: :cascade do |t|
-    t.string "name"
-    t.string "city"
-    t.string "state"
-    t.string "school_url"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "username"
-    t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
