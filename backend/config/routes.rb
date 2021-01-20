7305f1cb9f41a5646620545faba86dce4e4dcd6c
Sean M. Hoffman <me@seanmhoffman.com>
2021-01-20 05:14:24 -0600

    Updated Routes
    * Added index, search, and image actions from DogsController to `/api/v1` scope

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   scope 'api' do
     scope 'v1' do
+      get 'dog/breeds', to: 'dogs#index'
+      get 'dog/search', to: 'dogs#search'
+      get 'dog/image', to: 'dogs#image'
       resources :favorites
     end
   end
