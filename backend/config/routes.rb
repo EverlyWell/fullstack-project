abf9c813b8a5152a7cd8de572cb098754253059a
lilirdz <lilimrod@gmail.com>
2021-01-26 20:27:13 -0600

    fixed routing error

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
-    get 'gifs/search', to: 'gif#search'
+    #match controller action
+    get 'gifs/search', to: 'gifs#search_gifs'
     resources :favorites
 
   end
