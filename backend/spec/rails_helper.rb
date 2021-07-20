c90aea344e5a7a6187a6ed70444264dbc97e7b9a
Antt <anttyaadiggg@yahoo.com>
2021-07-20 02:30:31 -0400

    Shoulda gem setup

--- backend/spec/rails_helper.rb
+++ backend/spec/rails_helper.rb
@@ -1,6 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
+require 'shoulda/matchers'
 
 require File.expand_path('../config/environment', __dir__)
 
@@ -60,4 +61,10 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
 end
