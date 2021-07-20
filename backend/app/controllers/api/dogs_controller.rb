2dbda26d3f0af90bde4e639d458dc050abefc530
Antt <anttyaadiggg@yahoo.com>
2021-07-20 02:33:46 -0400

    Fav dogs GET and POST to api

--- backend/app/controllers/api/dogs_controller.rb
+++ backend/app/controllers/api/dogs_controller.rb
@@ -3,9 +3,29 @@ class Api::DogsController < ApplicationController
   def index
     @dog = DogApi.new.search(dog_params)
     if !@dog.nil?
-      render json: @dog
+    render json: @dog
     else
       render json: {message: "Cant load dogs"}
+    end
+  end
+
+  def favorite
+    user_id = user.id.to_s
+    @favorite = DogApi.new.favorite(user_id, favorite_params)
+    if !@favorite.nil?
+      render json: @favorite
+    else
+      render json: {message: "Cant load dogs"}
+    end
+  end
+
+  def user_favorites
+    user_id = user.id.to_s
+    @dog = DogApi.new.user_favorites(user_id)
+    if !@dog.nil?
+      render json: @dog
+    else
+      render json: {message: "Cant favorited dogs"}
     end
   end
 
@@ -13,5 +33,9 @@ class Api::DogsController < ApplicationController
 
   def dog_params
     params.permit(:breed_name)
+  end
+
+  def favorite_params
+    params.permit(:breed_id)
   end
 end
