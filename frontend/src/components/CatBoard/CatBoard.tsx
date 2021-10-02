a4d99043f8b5b74385367c4d4f8b2edf387f7ad1
Christian Ruink <christianruink@Christians-MacBook-Pro.local>
2021-10-02 14:37:31 -0700

    CatBoard story

--- frontend/src/components/CatBoard/CatBoard.tsx
+++ frontend/src/components/CatBoard/CatBoard.tsx
@@ -1,15 +1,88 @@
-
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import CatCard from '../CatCard/CatCard';
 import StyledComponents from './CatBoardStyled';
+import Spinner from 'react-spinners/ClipLoader';
 
 const {
     Container,
+    SearchInputContainer,
+    CatCardContainer,
+    SearchInput,
+    SearchText,
+    ButtonText,
+    ShowMyFavoritesButtonContainer,
+    Button,
+    SearchResultsContainer,
+    FavoritesResultsContainer,
+    ShowFavoritesContainer,
 } = StyledComponents;
 
 
+interface CatCardProps {
+    url: string,
+    id: string,
+}
+
 const CatBoard = () => {
+    const [searchInput, setSearchInput] = useState('');
+    const [isLoadingSearch, setIsLoadingSearch] = useState(false);
+
+    const [searchResults, setSearchResults] = useState([]);
+    const [favoritesResults, setFavorites] = useState([]);
+    const [isLoadingFaves, setIsLoadingFaves] = useState(false);
+
+    const handleSearch = () => {
+        setIsLoadingSearch(true);
+    }
+
+    const handleShowFavorites = () => {
+        setIsLoadingFaves(true);
+    }
+
+    const catCards = searchResults.map((catInfo : CatCardProps) => {
+        return (
+            <CatCardContainer>
+                <CatCard key={catInfo.id} {...catInfo} />
+            </CatCardContainer>
+        );
+    });
+
+    const favoriteCatCards = favoritesResults.map((catInfo : CatCardProps) => {
+        return (
+            <CatCardContainer>
+                <CatCard key={catInfo.id} {...catInfo} />
+            </CatCardContainer>
+        );  
+    });
+
     return (
-        <Container />
+        <Container>
+            <SearchInputContainer>
+                <SearchText>Search for CAT BREEDS!</SearchText>
+                <SearchInput 
+                    value={searchInput} 
+                    onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => setSearchInput(e.target.value)}
+                    onKeyPress={(e) => e.key === 'Enter' && handleSearch()}
+                    placeholder='bengal'
+                />
+                <Button onClick={handleSearch}>
+                    <ButtonText>SEARCH</ButtonText>
+                </Button>
+            </SearchInputContainer>
+            <SearchResultsContainer>
+                {isLoadingSearch ? (<Spinner loading={isLoadingSearch} color='#FFFFFF' />) : catCards}
+            </SearchResultsContainer>
+            <ShowFavoritesContainer>
+                <ShowMyFavoritesButtonContainer>
+                    <Button onClick={handleShowFavorites}>
+                        <ButtonText>Show my Favorites!</ButtonText>
+                    </Button>
+                </ShowMyFavoritesButtonContainer>
+            </ShowFavoritesContainer>
+            <FavoritesResultsContainer>
+                {isLoadingFaves ? (<Spinner loading={isLoadingFaves} color='#FFFFFF' />) : favoriteCatCards}
+            </FavoritesResultsContainer>
+        </Container>
     );
 };
 
--- frontend/src/components/CatBoard/CatBoardStyled.tsx
+++ frontend/src/components/CatBoard/CatBoardStyled.tsx
@@ -1,10 +1,94 @@
-
 import styled from 'styled-components';
 
-const Container = styled.div``;
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    padding: 20px;
+    background-color: #282c34;
+    min-width: 100vw;
+    min-height: 100vh;
+`;
+
+const CatCardContainer = styled.div`
+    margin: 40px;
+`;
+
+const SearchInputContainer = styled.div`
+    display: flex;
+    flex-direction: row; 
+    margin-bottom: 20px;    
+`;
+
+const SearchText = styled.div`
+    font-size: 24px;
+    color: white;
+`;
+
+const SearchInput = styled.input`
+    height: 30px;
+    width: 180px;
+    margin-left: 10px;
+    margin-right: 10px;
+`;
+
+
+const ShowMyFavoritesButtonContainer = styled.div`
+    cursor: pointer;
+`;
+
+const Button = styled.div`
+    display: flex;
+    background-color: gray;
+    border-color: white;
+    border-radius: 10px;
+    margin-bottom: 15px;
+    border-width: 2px;
+    border-style: solid;
+    height: 40px;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+`;
+
+const ButtonText = styled.div`
+    align-text: center;
+    font-size: 18px;
+    color: white;
+`;
+
+const ShowFavoritesContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+`;
+
+const FavoritesResultsContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    overflow-x: scroll;
+`;
+
+const SearchResultsContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    overflow-x: scroll;
+`;
 
 const StyledComponents = {
     Container,
+    CatCardContainer,
+    SearchInputContainer,
+    SearchInput,
+    SearchText,
+    ShowMyFavoritesButtonContainer,
+    Button,
+    ButtonText,
+    FavoritesResultsContainer,
+    SearchResultsContainer,
+    ShowFavoritesContainer,
 };
 
 export default StyledComponents;
+
