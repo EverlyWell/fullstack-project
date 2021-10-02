749aa912b2190470e3b43fc0061514a261a8ccaf
Christian Ruink <christianruink@Christians-MacBook-Pro.local>
2021-10-02 12:53:32 -0700

    Added authentication controller with JWT based scheme

---
+++ backend/app/controllers/api/authentication_controller.rb
@@ -0,0 +1,32 @@
+class Api::AuthenticationController < ApplicationController
+    # POST /auth/login
+    def login_or_create
+        user = User.find_by_username(params[:username])
+        if !user
+            @user = User.new(login_params)
+            @user.cat_api_sub_id = "demo-xas24t51_#{@user.username}"
+    
+            if @user.save
+            token = JsonWebToken.encode(user_id: @user.id)
+            time = Time.now + 24.hours.to_i
+            render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
+                            username: @user.username, cat_api_sub_id: @user.cat_api_sub_id }, status: :ok
+            else
+            render json: { errors: @user.errors.full_messages },
+                    status: :unprocessable_entity
+            end
+        elsif user&.authenticate(params[:password])
+            token = JsonWebToken.encode(user_id: user.id)
+            time = Time.now + 24.hours.to_i
+            render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
+                            username: user.username, cat_api_sub_id: user.cat_api_sub_id }, status: :ok
+        else
+            render json: { error: 'unauthorized' }, status: :unauthorized
+        end
+    end
+
+    def login_params
+        params.permit(:username, :password)
+    end  
+end
+
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
+    post '/auth/login', to: 'authentication#login_or_create'
+    get '/*a', to: 'application#not_found'     
   end
 end
