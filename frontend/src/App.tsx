9fc1a01830ebdac5ee624be3d09a247d0983ea85
Guillermo Siliceo <guillermo.siliceo@gmail.com>
2021-12-09 10:26:48 -0600

    Add Images Route to homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import './App.css';
 
+import { Link, Route } from "wouter";
+
+import Images from './pages/Images'
+
+const ImagesRoute = () => (
+    // wouter types are not playing nice with our props
+    // @ts-ignore
+    <Route path="/images/:query" component={Images} />
+)
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       <h1>
-            Cute collector
-       </h1>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>
+                    Cute collector
+                </h1>
+            </header>
+            
+            <ImagesRoute/> 
+        </div>
+    );
 }
 
 export default App;
