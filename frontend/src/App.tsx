9406ad884fc050e9f80e8c04a3403e78f672eec9
Guillermo Siliceo <guillermo.siliceo@gmail.com>
2021-12-09 13:55:53 -0600

    Add link to favorited images

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
 
-import { Route } from "wouter";
+import { Route, Link } from "wouter";
 
 import Images from './pages/Images'
+import Favs from './pages/Favs'
 
 import Search from './components/Search'
 
@@ -13,10 +14,22 @@ const ImagesRoute = () => (
     <Route path="/images/:query" component={Images} />
 )
 
+const FavedImagesRoute = () => (
+    // wouter types are not playing nice with our props
+    // @ts-ignore
+    <Route path="/favs" component={Favs} />
+)
+
 function App() {
 
     return (
         <div className="App">
+            <menu>
+                <Link href='/favs'>
+                    <a href='/favs'> Saved cuteness </a>
+                </Link>
+            </menu>
+
             <header className="App-header">
                 <h1>
                     Cute collector
@@ -25,6 +38,8 @@ function App() {
 
             <Search />
             
+
+            <FavedImagesRoute/>
             <ImagesRoute/> 
         </div>
     );
