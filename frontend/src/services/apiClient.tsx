f00fd5f1dd530b0b45eb02a7a2bda67edc738676
Guillermo Siliceo <guillermo.siliceo@gmail.com>
2021-12-09 13:54:55 -0600

    Add Favorited Images view

--- frontend/src/services/apiClient.tsx
+++ frontend/src/services/apiClient.tsx
@@ -1,6 +1,6 @@
 import { GIPHY_API_KEY, GIPHY_API_URL, API_URL } from './constants'
 
-const responseToImages = (response: any) => {
+const giphyResponseToImages = (response: any) => {
     const { data } = response
 
     const images = data.map((image: any) => {
@@ -11,8 +11,21 @@ const responseToImages = (response: any) => {
     return images
 }
 
+export function listSavedFavsIds() {
+    return fetch(API_URL, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+    })
+    .then((res) => res.json())
+    .then((response) => { return response.map((fav: any) => fav.giphy_id) })
+}
+
+export function getFavedImages() {
+    return listSavedFavsIds()
+        .then(getGiphyImagesById)
+}
+
 export function favImage(giphyId: string) {
-  // Default options are marked with *
   return fetch(API_URL+'?giphy_id='+giphyId, {
     method: 'POST',
     mode: 'no-cors',
@@ -23,12 +36,21 @@ export function favImage(giphyId: string) {
   });
 }
 
-export function getGiphyImages(keyword: string) {
-    const limit = 15;
-    const rating = 'g'; // kids friendly
-    const apiURL = `${GIPHY_API_URL}?api_key=${GIPHY_API_KEY}&q=${keyword}&limit=${limit}&rating=${rating}`
+export function getGiphyImagesById(ids: Array<string>) {
+    const idsString = ids.join(', ')
+    const apiURL = `${GIPHY_API_URL}?api_key=${GIPHY_API_KEY}&ids=${idsString}`
 
     return fetch(apiURL)
         .then((res) => res.json())
-        .then(responseToImages)
+        .then(giphyResponseToImages)
+}
+
+export function getGiphyImages(keyword: string) {
+    const limit = 15;
+    const rating = 'g'; // kids friendly
+    const apiURL = `${GIPHY_API_URL}/search?api_key=${GIPHY_API_KEY}&q=${keyword}&limit=${limit}&rating=${rating}`
+
+    return fetch(apiURL)
+        .then((res) => res.json())
+        .then(giphyResponseToImages)
 }
--- frontend/src/services/constants.tsx
+++ frontend/src/services/constants.tsx
@@ -1,3 +1,3 @@
 export const GIPHY_API_KEY = 'emptylVb5QDJB0iLQgRfpB6sRcSVL1Me';
-export const GIPHY_API_URL = 'https://api.giphy.com/v1/gifs/search';
-export const API_URL = 'http://localhost:3010/api/fav';
+export const GIPHY_API_URL = 'https://api.giphy.com/v1/gifs';
+export const API_URL = 'http://localhost:3010/api/favs';
